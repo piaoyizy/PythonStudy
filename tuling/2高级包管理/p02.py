ef3dc16ce6deec699912edc3e6b64a087d1c6dcc
zy <925754@qq.com>
2018-08-23 19:10:37 +0800

    图灵学习笔记

--- tuling/2高级包管理/p02.py
+++ tuling/2高级包管理/p02.py
@@ -1,6 +1,13 @@
-import  p01
-stu=p01.Student('张三',19)
-stu.say()
+# import  p01
+# stu=p01.Student('张三',19)
+# stu.say()
+#
+#
+# p01.SayHello()
 
 
-p01.SayHello()
+import importlib
+
+stu=importlib.import_module('p01')
+stu1 =stu.Student()
+stu1.say()
