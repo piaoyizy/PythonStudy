33252ecf441d096fe3e2ea47f65c4e8c42538f4e
zy <925754@qq.com>
2018-08-23 19:00:11 +0800

    2018.2.1激活

---
+++ tuling/2高级包管理/p01.py
@@ -0,0 +1,12 @@
+class Student():
+	def __init__(self,name='NoName',age=18):
+		self.name=name
+		self.age=age
+
+	def say(self):
+		print(self.name)
+
+def SayHello():
+	print('SayHello')
+
+
---
+++ tuling/2高级包管理/p02.py
@@ -0,0 +1,6 @@
+import  p01
+stu=p01.Student('张三',19)
+stu.say()
+
+
+p01.SayHello()
