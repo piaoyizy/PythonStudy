a4b758c1b331321dff7ba199c78a2f6b615269d7
zy <925754@qq.com>
2018-08-28 21:24:15 +0800

    t

--- tuling/file/f01.py
+++ tuling/file/f01.py
@@ -9,7 +9,7 @@ f.close()
 
 ##读取文件内容
 with open(r'text.txt', 'r') as f:
-	strline = f.readline(100)  # 入参：N行
+	strline = f.readline(1)  # 入参：N行
 	while strline:
 		print(strline)
 		strline = f.readline()
@@ -22,13 +22,31 @@ with open(r'text1.txt', 'r') as f:
 	print(strchar)
 
 print('--' * 10)
-# seek
-with open(r'text.txt', 'r') as f:
-	strChar = f.read(3)
-	pos=f.tell()
+# 每次读取三个字符
+with open(r'text.txt') as f:
+	strChar = f.read(30)  # 30个字符
+	pos = f.tell()
 	while strChar:
-		print('---'+strChar.strip()+'---')
+		print('---' + strChar.strip() + '---')
 		print(f.tell())
 		sleep(0.1)
-		strChar = f.read(3)
+		strChar = f.read(30)
 		pos = f.tell()
+
+# 序列化 经常用到，重要
+import pickle
+
+age = 19
+with open(r'test03xu.txt', 'wb')as f:
+	pickle.dump(age, f)
+with open(r'test03xu.txt', 'rb')as f:
+	print('反实例化:{0}'.format(pickle.load(f)))
+
+a = [19, 'zhangyao', 'i love zhangyucheng', [185, 86]]
+with open(r'test03xu.txt', 'wb')as f:
+	pickle.dump(a,f)
+with open(r'test03xu.txt', 'rb')as f:
+	print('反实例化:{0}'.format(pickle.load(f)))
+
+
+
